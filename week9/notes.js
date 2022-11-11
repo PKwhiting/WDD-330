dd88f4b4e75c4bddb0d56ceb9c52b1a97bff36d3
PKwhiting <patten.whiting@gmail.com>
2022-11-11 10:10:11 -0700

    week 9

---
+++ week9/notes.js
@@ -0,0 +1,32 @@
+/*
+- The window object is the global object within the browser.
+- Global variables are actually properties of widgets in the browser environment.
+- Can global window variables be passed to JS and can they also be passed between pages?
+- alert, Confirm(), and Prompt() are widget methods that open a dialog that stops program execution.
+- The window.navigator object provides information about the user's browser and operating system, but it can be unreliable.
+- Can you use window.navigator to pull the relative size of the screen?
+- Which browsers use window.navigator, compatability?
+- The window.location object provides information about the URL of the current page.
+- The window.history object stores information about pages visited in a session.
+- You can open, close, resize, and move windows (but that doesn't mean you should!).
+- The window.screen object provides information about the user's screen.
+- document.write() is an archaic method for writing text to a document and should be avoided.
+- Cookies can be used to store pieces of information that are small between requests using the document.cookie property.
+- How do you delete a cookie when a user doesn't want it used anymore?
+- You can use the window.setTimeout() method to call a function after a specified amount of time. It can be aborted with the clearTimeout() method.
+- You can use the window.setInterval() method to call a function repeatedly. You can stop it with the clearInterval() method.
+- The window.requestAnimationFrame() method lets you use your browser's built-in graphics capabilities to create smooth, optimized animations. It can be canceled with the cancelAnimationFrame() method.
+- HTML5.1 is the latest version of the Hypertext Markup Language. It covers a wide variety of technologies, including multiple APIs accessible via JavaScript.
+- Data attributes are useful for embedding custom data in web pages. You can use this to improve your JavaScript user experience.
+- Can the position object be used to update location constantly, for some kind of mobile game for example?
+- The Web Storage API allows key-value pairs to be stored on the user's device in a manner similar to cookies, but without the same storage limits.
+- The Geolocation API allows access to the geo coordinates of the user's device as long as the user gives permission.
+- You can use the Web Worker API to run computationally intensive tasks in the background and prevent your website from becoming unresponsive.
+- Websockets is a new protocol for communicating over the Internet, allowing real-time two-way communication.
+- Is the "push" communication of websocket a similar concept to github?
+- The Notifications API allows notifications to be displayed on the user's system.
+- You can embed audio tracks and video clips in your web pages using the also has a Media API that helps you control playback using JavaScript.
+- The Canvas element allows you to dynamically draw geometric shapes, text, and images on your web page in real time using JavaScript.
+- A shim or polyfill is code that adds support for missing features in older browsers.
+- Are there certain limits on file types for displaying multimedia?
+*/
